44bddde966e2cb559b78e835403d734c071d68d3
H4Mza Sheib4 <107404879+DeveloperOnTrip@users.noreply.github.com>
2022-08-03 18:28:29 +0400

    ScoreKeeper

--- King Island/Assets/Script/Health/BoxHealth.cs
+++ King Island/Assets/Script/Health/BoxHealth.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class BoxHealth : MonoBehaviour
 {
+    ScoreKeeper _scoreKeeper;
+
+    private void Awake()
+    {
+        _scoreKeeper = FindObjectOfType<ScoreKeeper>();
+    }
+
     public void DestroyMe()
     {
+        _scoreKeeper.modifyScore(+1);
         Destroy(gameObject);
     }
 }
